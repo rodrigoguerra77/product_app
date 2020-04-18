e27ddc7ada20e35a20c131d65b6f054978deba8e
Rodrigo Guerra <rodrigoguerra77@gmail.com>
2020-04-18 18:47:59 -0400

    Forms of Introo to Vue.js

--- main.js
+++ main.js
@@ -1,3 +1,86 @@
+Vue.config.devtools = true
+
+Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+
+        <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </p>
+
+        <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name" placeholder="name">
+        </p>
+        
+        <p>
+            <label for="review">Review:</label>      
+            <textarea id="review" v-model="review"></textarea>
+        </p>
+        
+        <p>
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating">
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+        </p>
+
+        <p>Would you recommend this product?</p>
+        <label>
+          Yes
+          <input type="radio" value="Yes" v-model="recommend"/>
+        </label>
+        <label>
+          No
+          <input type="radio" value="No" v-model="recommend"/>
+        </label>
+            
+        <p>
+            <input type="submit" value="Submit">  
+        </p>    
+    
+    </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            recommend: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if(this.name && this.review && this.rating && this.recommend){
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend,
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null,
+                this.review = null,
+                this.rating = null,
+                this.recommend = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.recommend) this.errors.push("Recommendation required.")
+            }
+        }
+    }
+})
+
 Vue.component('product-details', {
     props: {
         details: {
@@ -65,6 +148,21 @@ Vue.component('product', {
             
             <button @click="removeFromCart">Remove from Cart</button>
 
+            <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There are not reviews yet.</p>
+                <ul>
+                    <li v-for="(review, index) in reviews" :key="index">
+                        <p>{{ review.name }}</p>
+                        <p>Raiting: {{ review.review }}</p>
+                        <p>{{ review.rating }}</p>
+                        <p>Recommend: {{ review.recommend }}</p>
+                    </li>
+                </ul>
+            </div>
+
+            <product-review @review-submitted="addReview"></product-review>
+
         </div>
 
     </div>
@@ -92,8 +190,8 @@ Vue.component('product', {
                     variantQuantity: 0
                 }
             ],
-            sizes: ['small', 'medium', 'large', 'extra large']
-            
+            sizes: ['small', 'medium', 'large', 'extra large'],
+            reviews: []
         }
     },
     methods: {
@@ -105,6 +203,9 @@ Vue.component('product', {
         },
         removeFromCart: function() {
             this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
